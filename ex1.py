2cf94fc79962676e3ce81f7802d62e1d7fbf08c7
mckay1818 <72710253+mckay1818@users.noreply.github.com>
2022-02-18 17:06:08 -0500

    initial commit

---
+++ ex1.py
@@ -0,0 +1,8 @@
+hilarious = False
+joke_eval = "Isnt't that joke so funny?! {}"
+print(joke_eval.format(hilarious))
+
+end1 = "ste"
+end2 = "wy"
+print(end1, end=" ")
+print(end2)
---
+++ format_practice.py
@@ -0,0 +1,8 @@
+end1 = "C"
+end2 = "o"
+end3 = "u"
+end4 = "r"
+end5 = "t"
+
+print(end1 + end2 + end3 + end4 + end5)
+print(end1 + end2)
